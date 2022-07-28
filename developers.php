66b7eecf666cf299fbfc83b085a81bdb27a703ad
Danny-LLi <dannysafaya@gmail.com>
2022-07-28 07:38:08 +0300

    my first commit

---
+++ developers.php
@@ -0,0 +1,112 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Send Money, Pay Online, or Set Up a Merchant Account.</title>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</head>
+
+<body>
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img style="height:700px;"src="img/person_1.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img style="height:700px;"src="img/person_2.jpg" class="d-block w-100" alt="...">
+    </div>
+    
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+    
+    <script>// Pages
+document.querySelectorAll('.logo').forEach(logo => {
+    logo.addEventListener('click', () => {
+        document.querySelector('.front-page').style.display = 'block'
+        document.querySelector('.login-page').style.display = 'none'
+        document.querySelector('.signup-page').style.display = 'none'
+    })
+})
+
+document.querySelectorAll('.login').forEach(loginBtn => {
+    loginBtn.addEventListener('click', () => {
+        document.querySelector('.front-page').style.display = 'none'
+        document.querySelector('.login-page').style.display = 'block'
+        document.querySelector('.signup-page').style.display = 'none'
+    })
+})
+
+document.querySelectorAll('.signup').forEach(signupBtn => {
+    signupBtn.addEventListener('click', () => {
+        document.querySelector('.front-page').style.display = 'none'
+        document.querySelector('.login-page').style.display = 'none'
+        document.querySelector('.signup-page').style.display = 'flex'
+    })
+})
+// End of Pages
+
+// Navigation
+const dropdownItems = document.querySelectorAll('.dropdown-hover')
+
+if (window.innerWidth < 1000) {
+    const menuIcon = document.querySelector('.menu')
+    const navbar = document.querySelector('.navbar')
+
+    menuIcon.addEventListener('click', () => {
+        navbar.classList.toggle('change')
+
+        if (!navbar.classList.contains('change')) {
+            document.querySelectorAll('.nav-dropdown').forEach(dropdown => {
+                dropdown.style.left = '-20rem'
+            })
+        }
+    })
+
+    document.querySelectorAll('.show-dropdown').forEach(link => {
+        link.addEventListener('click', () => {
+            link.nextElementSibling.style.left = '0'
+        })
+    })
+
+    document.querySelectorAll('.dropdown-heading-link').forEach(headingLink => {
+        headingLink.addEventListener('click', () => {
+            headingLink.parentElement.parentElement.style.left = '-20rem'
+        })
+    })
+} else {
+    dropdownItems.forEach(dropdownItem => {
+        dropdownItem.addEventListener('mouseover', () => {
+            dropdownItem.lastElementChild.style.cssText = 'opacity: 1; visibility: visible'
+            document.querySelector('.navbar-wrapper').style.background = 'linear-gradient(to right, #066399, #2f8fdf, #066399)'
+            dropdownItem.firstElementChild.firstElementChild.style.transform = 'rotate(180deg)'
+        })
+        dropdownItem.addEventListener('mouseout', () => {
+            dropdownItem.lastElementChild.style.cssText = 'opacity: 0; visibility: hidden'
+            document.querySelector('.navbar-wrapper').style.background = 'none'
+            dropdownItem.firstElementChild.firstElementChild.style.transform = 'rotate(0)'
+        })
+    })
+}
+
+window.addEventListener('resize', () => {
+    // window.location.reload()
+})
+
+// End of Navigation</script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Send Money, Pay Online, or Set Up a Merchant Account.</title>
+    <link rel="stylesheet" type="text/css" href="css/style1.css? <?php echo time(); ?> "/>
+    <link rel="stylesheet" href="css/style3.css">
+    <!-- CSS only -->
+<!-- JavaScript Bundle with Popper -->
+    
+</head>
+
+<body>
+
+    <div class="container">
+        <!-- Front Page -->
+        <div class="front-page">
+            <!-- Section 1 -->
+            <section class="section-1">
+                <div class="navbar-wrapper">
+                    <nav class="navbar">
+                        <div class="menu">
+                            <div class="menu-icon">
+                                <div class="line line-1"></div>
+                                <div class="line line-2"></div>
+                                <div class="line line-3"></div>
+                            </div>
+                            <span>Menu</span>
+                        </div>
+                        <a href="#"> <div class="navbar-logo logo">
+                            <span class="p-1">PG</span>
+                            <span class="p-2">PG</span>
+                            PaymentGateway
+                        </div></a>
+                        <ul class="nav-list">
+                            <li class="nav-list-item dropdown-hover">
+                                <a href="#" class="nav-list-link show-dropdown">Personal <i
+                                        class="fas fa-chevron-down"></i></a>
+                                <ul class="nav-dropdown nav-dropdown-personal">
+                                    <li class="nav-dropdown-item dropdown-heading">
+                                        <a href="#" class="dropdown-heading-link">
+                                            <i class="fas fa-chevron-down"></i> Personal
+                                        </a>
+                                    </li>
+                                   
+
+                                    <li class="nav-dropdown-item">
+                                        <a href="#" class="nav-dropdown-link-1">Send payments</a>
+                                        <a href="#" class="nav-dropdown-link-2">Send payments abroad</a>
+                                    </li>
+
+                                </ul>
+                                
+                            </li>
+                            <li class="nav-list-item dropdown-hover">
+                                <a href="#" class="nav-list-link show-dropdown">Business <i
+                                        class="fas fa-chevron-down"></i></a>
+                                <ul class="nav-dropdown nav-dropdown-business">
+                                    <li class="nav-dropdown-item dropdown-heading">
+                                        <a href="#" class="dropdown-heading-link">
+                                            <i class="fas fa-chevron-down"></i> Business
+                                        </a>
+                                    </li>
+                                    <li class="nav-dropdown-item">
+                                        <a href="#" class="nav-dropdown-link-1">Accept online payments</a>
+                                        <a href="#" class="nav-dropdown-link-2">Get paid on your website</a>
+                                    </li>
+
+                                </ul>
+                            </li>
+                            <li class="nav-list-item dropdown-hover">
+                                <a href="#" class="nav-list-link show-dropdown">Partners and Developers <i
+                                        class="fas fa-chevron-down"></i></a>
+                                <ul class="nav-dropdown nav-dropdown-personal">
+                                    <li class="nav-dropdown-item dropdown-heading">
+                                        <a href="#" class="dropdown-heading-link">
+                                            <i class="fas fa-chevron-down"></i> Personal
+                                        </a>
+                                    </li>
+                                   
+
+                                    <li class="nav-dropdown-item">
+                                        <a href="#" class="nav-dropdown-link-1">Danny Safaya</a>
+                                        <a href="#" class="nav-dropdown-link-1">laith Tabliea</a>
+                                    </li>
+
+                                </ul>
+                                
+                            </li>
+                            
+                        </ul>
+                        <div class="navbar-buttons"><a href="#"><button class="navbar-btn login-btn login">Login</button></a>
+                            
+                        <a href="#"><button class="navbar-btn signup-btn signup"style="background-color: #fff;color:black;">Signup</button></a>
+                        </div>
+                    </nav>
+                </div>
+                <div class="banner">
+                    <h1 class="banner-heading">your Best Choice</h1>
+                    <p class="banner-paragraph">You will facilitate the transfer your funds via the Internet through the
+                        electronic payment
+                        gateway.</br>Your security is important to us.</p>
+                    <a href="/github/Renewable-Systems-Corporation1/learnmore"><button class="blue-btn">Learn More</button></a>
+                    <a href="/github/Renewable-Systems-Corporation1/developers"><button class="blue-btn">Developers</button></a>
+
+
+                </div>
+            </section>
+            <!-- End of Section 1 -->
+
+            <!-- Section 2 -->
+            <section class="section-2">
+                <h1 class="section-2-heading">PaymentGateway is for everyone who pays or gets paid</h1>
+                <div class="services">
+                    <div class="service">
+                        <i class="fas fa-user-tie"></i>
+                        <h3 class="service-heading">Individuals</h3>
+                        <p class="service-paragraph">You can receive your funds or pay.</p>
+                        <a href="individuals"><button class="service-btn">Learn More</button></a>
+                    </div>
+                    <div class="service">
+                        <i class="fas fa-briefcase"></i>
+                        <h3 class="service-heading">Businesses</h3>
+                        <p class="service-paragraph">Accepting multiple payment methods like credit cards and mobile
+                            payment apps.</p>
+                            <a href="businesses"><button class="service-btn">Learn More</button></a>
+                    </div>
+
+                </div>
+            </section>
+            <!-- End of Section 2 -->
+
+            <!-- Section 3 -->
+            <section class="section-3">
+                <h1 style="width: max-content;margin: 0 auto 1rem auto;"class="section-3-heading">The world prefers </br>PaymentGateway</br></h1>
+               
+                <div style="text-align:center;"class="container text-center">
+                <div class="row">
+                 
+                    <div class="col">
+                    <h3 class="feature-heading">Fast Payments</h3>
+                        <p class="feature-paragraph">Fast is needed to make you feel good.</p>
+                    </div>
+                    <div class="col">
+                    <h3 class="feature-heading">Global Reach</h3>
+                        <p class="feature-paragraph">Out service is used by a large numbers of people.</p>
+                    </div>
+                    <div class="col">
+                    <h3 class="feature-heading">Easy Integration</h3>
+                        <p class="feature-paragraph">Easy to use.</p>
+                    </div>
+                    <div class="col">
+                    <h3 class="feature-heading">Advanced Protection</h3>
+                        <p class="feature-paragraph">We use the Global protection standards.</p>
+                    </div>
+                    <div class="col">
+                    <h3 class="feature-heading">24/7 Service</h3>
+                        <p class="feature-paragraph">We will not shutdown our service</p>
+                    </div>
+                </div>
+                </div>
+               
+          
+            </section>
+            <!-- End of Section 3 -->
+            <!-- Section 4 -->
+            <section class="section-4">
+                <h1 class="section-4-heading">Get started with PaymentGateway.</h1>
+                <div class="section-4-content">
+                    <div class="section-4-img-wrapper">
+                        <img src="img/macbook.png">
+                    </div>
+                    <ul class="section-4-list">
+                        <li class="section-4-list-item">
+                            <span class="number">1</span>
+                            <span>Set up <a href="#">a business account</a></span>
+                        </li>
+                        <li class="section-4-list-item">
+                            <span class="number">2</span>
+                            <span>No Scamming, No fraud, No Threats.</span>
+                        </li>
+                        <li class="section-4-list-item">
+                            <span class="number">3</span>
+                            <span>Every Thing will be secure.</span>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+            <!-- End of Section 4 -->
+
+            <!-- Section 5 -->
+            <section class="section-5">
+                <div class="section-5-top">
+                    <h2 class="section-5-top-heading">Sign Up and Get Started</h2>
+                    <a href="#"><button class="blue-btn signup">Get Started</button></a>
+                </div>
+                <footer class="footer">
+                    <div class="footer-top">
+                    </div>
+                    <div class="footer-bottom">
+                    <p class="copyright">Copyright &copy; All Rights Reserved</p>
+                        
+                    </div>
+                </footer>
+            </section>
+            <!-- End of Section 5 -->
+        </div>
+        <!-- End of Front Page -->
+
+    </div>
+ 
+
+    <script>// Pages
+document.querySelectorAll('.logo').forEach(logo => {
+    logo.addEventListener('click', () => {
+        document.querySelector('.front-page').style.display = 'block'
+        document.querySelector('.login-page').style.display = 'none'
+        document.querySelector('.signup-page').style.display = 'none'
+    })
+})
+
+document.querySelectorAll('.login').forEach(loginBtn => {
+    loginBtn.addEventListener('click', () => {
+        document.querySelector('.front-page').style.display = 'none'
+        document.querySelector('.login-page').style.display = 'block'
+        document.querySelector('.signup-page').style.display = 'none'
+    })
+})
+
+document.querySelectorAll('.signup').forEach(signupBtn => {
+    signupBtn.addEventListener('click', () => {
+        document.querySelector('.front-page').style.display = 'none'
+        document.querySelector('.login-page').style.display = 'none'
+        document.querySelector('.signup-page').style.display = 'flex'
+    })
+})
+// End of Pages
+
+// Navigation
+const dropdownItems = document.querySelectorAll('.dropdown-hover')
+
+if (window.innerWidth < 1000) {
+    const menuIcon = document.querySelector('.menu')
+    const navbar = document.querySelector('.navbar')
+
+    menuIcon.addEventListener('click', () => {
+        navbar.classList.toggle('change')
+
+        if (!navbar.classList.contains('change')) {
+            document.querySelectorAll('.nav-dropdown').forEach(dropdown => {
+                dropdown.style.left = '-20rem'
+            })
+        }
+    })
+
+    document.querySelectorAll('.show-dropdown').forEach(link => {
+        link.addEventListener('click', () => {
+            link.nextElementSibling.style.left = '0'
+        })
+    })
+
+    document.querySelectorAll('.dropdown-heading-link').forEach(headingLink => {
+        headingLink.addEventListener('click', () => {
+            headingLink.parentElement.parentElement.style.left = '-20rem'
+        })
+    })
+} else {
+    dropdownItems.forEach(dropdownItem => {
+        dropdownItem.addEventListener('mouseover', () => {
+            dropdownItem.lastElementChild.style.cssText = 'opacity: 1; visibility: visible'
+            document.querySelector('.navbar-wrapper').style.background = 'linear-gradient(to right, #066399, #2f8fdf, #066399)'
+            dropdownItem.firstElementChild.firstElementChild.style.transform = 'rotate(180deg)'
+        })
+        dropdownItem.addEventListener('mouseout', () => {
+            dropdownItem.lastElementChild.style.cssText = 'opacity: 0; visibility: hidden'
+            document.querySelector('.navbar-wrapper').style.background = 'none'
+            dropdownItem.firstElementChild.firstElementChild.style.transform = 'rotate(0)'
+        })
+    })
+}
+
+window.addEventListener('resize', () => {
+    // window.location.reload()
+})
+
+// End of Navigation</script>
+</body>
+
+</html>
